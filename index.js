347200f3ff5201dd24cb8b3970be3381c1cb158c
LITHISHWARAN <lithishwaran.v.s75@kalvium.community>
2025-02-13 10:10:55 +0530

    database connected

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { resolve } = require('path');
-
+require("dotenv").config();
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+const uri = process.env.MONGO_DB
+const mongoose = require("mongoose")
+mongoose
+.connect(uri)
+.then(()=>{console.log("Connect to database")})
+.catch((err)=>console.log(err))
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
